56a433a3038456f94e8a4b22e872d0a63926be69
Prince Joel <prince.joel441@gmail.com>
2021-01-25 03:23:55 +0530

    Created two tab view.

---
+++ app/src/main/java/com/example/vehicle/presentation/view/TwoTabFragment.java
@@ -0,0 +1,71 @@
+package com.example.vehicle.presentation.view;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.vehicle.R;
+import com.google.android.material.tabs.TabLayout;
+
+public class TwoTabFragment extends Fragment {
+
+
+    View myFragment;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+
+    public TwoTabFragment(){
+        //Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        myFragment= inflater.inflate(R.layout.fragment_two_tab, container, false);
+
+        viewPager = myFragment.findViewById(R.id.viewpager);
+        tabLayout = myFragment.findViewById(R.id.tab_layout);
+
+        return myFragment;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        setUpViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+    private void setUpViewPager(ViewPager viewPager) {
+        PageAdapter adapter =new PageAdapter(getChildFragmentManager());
+
+        adapter.addFragment(new FanTabFragment(),"Front");
+        adapter.addFragment(new SeatHeaterTabFragment(),"Seat");
+
+        viewPager.setAdapter(adapter);
+    }
+}
